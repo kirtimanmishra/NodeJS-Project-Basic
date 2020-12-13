8e772402fdb75ac8066ddb02d0a7f78d567d607f
kirtiman mishra <mishrakirtiman24@gmail.com>
2020-12-14 01:10:44 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const blogRoutes = require('./routes/blogRoutes');
+
+// express app
+const app = express();
+
+// connect to mongodb & listen for requests
+const dbURI = "mongodb+srv://kirti:kirti123@nodejs.worru.mongodb.net/nodejs_db?retryWrites=true&w=majority";
+
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(result => app.listen(80))
+  .catch(err => console.log(err));
+
+// register view engine
+app.set('view engine', 'ejs');
+
+// middleware & static files
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+
+// routes
+app.get('/', (req, res) => {
+  res.redirect('/blogs');
+});
+
+app.get('/about', (req, res) => {
+  res.render('about', { title: 'About' });
+});
+
+// blog routes
+app.use('/blogs', blogRoutes);
+
+// 404 page
+app.use((req, res) => {
+  res.status(404).render('404', { title: '404' });
+});
